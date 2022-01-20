16d1a51e356b17bf5735635f81bfb7f77b606163
rohitgeddam <rohitgeddam2018@gmail.com>
2022-01-20 16:20:07 +0530

    setup materialize css

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from ideas import views as ideas_views
+
+ideas_patterns = [
+    path("", ideas_views.Home, name="home")
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include((ideas_patterns, 'ideas')))
 ]
+

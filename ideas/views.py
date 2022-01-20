590a9fa6e09db772c9dc3a7f921812f9e060a439
rohitgeddam <rohitgeddam2018@gmail.com>
2022-01-20 18:48:00 +0530

    add floating button to add new idea

--- ideas/views.py
+++ ideas/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from ideas.forms import IdeaForm
 
 # Create your views here.
+@login_required()
 def home(request):
-    return render(request, "ideas/home.html")
+    ideaform = IdeaForm()
+
+    if request.method == "POST":
+        ideaform = IdeaForm(request.POST)
+
+        if ideaform.is_valid():
+            idea = ideaform.save(commit=False)
+            idea.author = request.user
+            idea.save()
+            messages.add_message(request, messages.SUCCESS, 'New Idea Posted')
+            return redirect("ideas:home")
+        else:
+            messages.add_message(request, messages.ERROR, 'There was a problem while posting idea')
+
+
+    return render(request, "ideas/home.html", { 'ideaform': ideaform })
 
 def create_view(request):
+
     return render(request, "ideas/create.html")
 
 def update_view(request, slug):
